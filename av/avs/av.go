4cc2fefcbc3ad29d7c84db1f7e898e1ba6739a16
Alesya Moseva <mosevaa13@gmail.com>
2023-04-18 22:42:53 +0300

    add signatures structure

--- av/avs/av.go
+++ av/avs/av.go
@@ -1,3 +1,17 @@
 package avs
 
 //здесь все, что касается антивируса, сигнатур и тд
+
+type AVS struct {
+	signatures map[string]Signature
+	VirusStats map[string][]string
+}
+
+type Signature struct {
+	id          int64
+	sign        []byte
+	sha         string
+	offsetBegin string
+	offsetEnd   string
+	dtype       string
+}
