89a7764862cf162a35bd59870f180c1e09979af0
Andrew Sergeev <wantedmins@gmail.com>
2023-04-23 20:39:29 +0300

    UP

--- ui/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-# записываем данные в stdout
-sys.stdout.write("Hello, World!\n")
-sys.stdout.flush()  # очищаем буфер
-
-# читаем данные из stdin
-data = sys.stdin.read()
-print("Input:", data)
