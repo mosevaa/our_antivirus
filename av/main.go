17d995844f3c7fa10d45168a8eda955ccbce8c62
Andrew Sergeev <wantedmins@gmail.com>
2023-04-29 02:59:13 +0300

    UP

--- av/main.go
+++ av/main.go
@@ -47,15 +47,16 @@ func run() error {
 	ptree := searchtree.GetSearchTree()
 	log.Println("Prefix tree loaded")
 
-	// fmt.Printf("my cmd: \"%v\"\n", string(*cmd))
-
 	cmd := flag.String("cmd", "", "")
 	flag.Parse()
 
 	if strings.Contains(string(*cmd), "scan_all") {
 		fmt.Println("scan_all")
-		// вот тут указать папку или файл
-		if err := avs.FindSignatures("/Users/andreysergeev/Documents/GitHub/antivirus/our_antivirus/folder", ptree); err != nil {
+
+		link := string(*cmd)[9:len(string(*cmd))]
+		fmt.Println(link)
+		// вот тут указать  файл
+		if err := avs.FindSignatures(link, ptree); err != nil {
 			return err
 		}
 		printResults()
@@ -63,31 +64,9 @@ func run() error {
 	} else if strings.Contains(string(*cmd), "quarantine") {
 		fmt.Println("quarantine")
 		//scan.Restore("quarantine/file.txt", "../folder/file.txt")
-	} else if strings.Contains(string(*cmd), "scan_one_file") {
-		fmt.Println("scan_one_file")
-		//scan.ScanFile("//path_to_file")
-	} else if strings.Contains(string(*cmd), "start") {
-		fmt.Println("start")
-	} else if strings.Contains(string(*cmd), "stop") {
-		fmt.Println("stop")
 	} else {
 		fmt.Println("error")
 	}
-
-	// if len(os.Args) > 1 {
-	// 	for index, scanDir := range os.Args {
-	// 		if index == 0 {
-	// 			continue
-	// 		}
-
-	// 		log.Println("Start search in location:", scanDir)
-	// 		if err := avs.FindSignatures(scanDir, ptree); err != nil {
-	// 			return err
-	// 		}
-
-	// 		printResults()
-	// 	}
-	// }
 
 	return nil
 }
